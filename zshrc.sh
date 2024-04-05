caf06989b34c41603c58d944ec17b2374d4d7123
unbrain <marsorsun@gmail.com>
2024-04-05 18:22:00 +0800

    add sh

---
+++ zshrc.sh
@@ -0,0 +1,23 @@
+# !/bin/sh
+
+_() {d
+  ZSH="~/.zshrc"
+
+  echo '
+  # git
+  alias ga.="git add ."
+  alias gc="git commit"
+  alias gsb="git status -sb"
+  alias gp="git push"
+  alias gco="git checkout"
+
+  # proxy
+  alias w2proxy="export https_proxy=http://127.0.0.1:8899 http_proxy=http://127.0.0.1:8899 all_proxy=socks5://127.0.0.1:8899"
+  alias clashproxy="export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890"
+
+  ' >> '${ZSH}'
+
+  source '${ZSH}'
+} && _
+
+unset -f _

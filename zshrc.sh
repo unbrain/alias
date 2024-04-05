41b489f1abdb770cca8a11bd16a1101c8fe88ec9
unbrain <marsorsun@gmail.com>
2024-04-05 18:40:33 +0800

    add sh

--- zshrc.sh
+++ zshrc.sh
@@ -4,23 +4,22 @@ _() {
   # echo "where is yourzshrc"
   # read -r ZSH
   # [ -z "$ZSH" ] && exit 1
-  ZSH="~/.zshrc"
+  # ZSH='~/.zshrc'
 
   echo '
-  # git
-  alias ga.="git add ."
-  alias gc="git commit"
-  alias gsb="git status -sb"
-  alias gp="git push"
-  alias gco="git checkout"
+# git
+alias ga.="git add ."
+alias gc="git commit"
+alias gsb="git status -sb"
+alias gp="git push"
+alias gco="git checkout"
 
-  # proxy
-  alias w2proxy="export https_proxy=http://127.0.0.1:8899 http_proxy=http://127.0.0.1:8899 all_proxy=socks5://127.0.0.1:8899"
-  alias clashproxy="export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890"
+# proxy
+alias w2proxy="export https_proxy=http://127.0.0.1:8899 http_proxy=http://127.0.0.1:8899 all_proxy=socks5://127.0.0.1:8899"
+alias clashproxy="export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890"
+' >> ~/.zshrc
 
-  ' >> '${ZSH}'
-
-  source '${ZSH}'
+  source ~/.zshrc
 } && _
 
 unset -f _
